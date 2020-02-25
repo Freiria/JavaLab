901fd17b47b566bb464334488a862e51ade28bf4
Allan Freiria <afreiriatech@gmail.com>
2020-02-24 22:45:33 -0300

    add revisao poo, pendente resolucao

---
+++ revisaoPoo/src/revisaopoo/RevisaoPoo.java
@@ -0,0 +1,21 @@
+
+package revisaopoo;
+
+/*
+Faça uma programa que receba 10 idades,
+pesos e alturas, calcule e mostre:
+– A média de idade das 10 pessoas
+– A quantidade de pessoas com peso superior a
+90 kg e altura inferior a 1,50 metro
+– A porcentagem de pessoas com idade entre
+10 e 30 anos entre as pessoas com mais de
+1,90 metro de altura
+*/
+
+public class RevisaoPoo {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
